9e02f2ac93f9d07f31469f8d2200888663839468
kaknjo.harun2019@gmail.com <kaknjo.harun2019@gmail.com>
2020-12-02 00:42:03 +0100

    Home Controller with dependency injection

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -3,12 +3,23 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using EmployeeMenagment.Models;
 
 namespace EmployeeMenagment.Controllers 
 {
     public class HomeController : Controller{
-        public JsonResult Index(){
-            return Json(new {id=1, name="Harun"});
+        private  readonly IEmployeeRepository _employeeRepository;
+        public HomeController(IEmployeeRepository employeeRepository)
+        {
+            _employeeRepository=employeeRepository;
+        }
+        public String Index(){
+                return _employeeRepository.GetEmployee(1).Name;
+        }
+
+        public ViewResult Details(){
+                Employee model= _employeeRepository.GetEmployee(1);
+                return View(model);
         }
     }
 
--- Startup.cs
+++ Startup.cs
@@ -17,6 +17,8 @@ namespace EmployeeMenagment
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvcCore(option => option.EnableEndpointRouting = false);
+           
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -27,16 +29,13 @@ namespace EmployeeMenagment
                 app.UseDeveloperExceptionPage();
             }
 
-            app.UseRouting();
             app.UseStaticFiles();
-            
 
-            app.UseEndpoints(endpoints =>
+            app.UseMvcWithDefaultRoute();
+
+            app.Run(async (context) =>
             {
-                endpoints.MapGet("/", async context =>
-                {
-                    await context.Response.WriteAsync("Hello World!");
-                });
+                await context.Response.WriteAsync("Hello World!");
             });
         }
     }
