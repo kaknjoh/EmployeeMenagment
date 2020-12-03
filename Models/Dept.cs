35b0cde19742667c26b624eb103a8c532ed1a2ca
kaknjo.harun2019@gmail.com <kaknjo.harun2019@gmail.com>
2020-12-04 00:12:30 +0100

    Form for employee creating

---
+++ Models/Dept.cs
@@ -0,0 +1,16 @@
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EmployeeMenagment.Models {
+
+    public enum Dept
+    { 
+        None, 
+        Hr, 
+        IT, 
+        Payroll
+    }
+}
